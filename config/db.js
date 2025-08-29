6c64864c94db796a761b03e8d114156f093e2067
UjjwalFullStackDev <ujjwal.tyagi90611@gmail.com>
2025-08-29 22:36:38 +0530

    Fix DB integration

--- config/db.js
+++ config/db.js
@@ -7,14 +7,26 @@ const sequelize = new Sequelize(
     process.env.DB_PASS,
 {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     logging: false,
+    dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            },
+            connectTimeout: 60000
+        },
     pool: {
         max: 5,
         min: 0,
         acquire: 30000,
         idle: 10000
-    }
+    },
+    retry: {
+            max: 3,
+            timeout: 60000
+        }
 }
 )
 
